d143b3fc25476f5e18128ff46b292f420ac42d2e
Disha-arch <guptadisha.1009@gmail.com>
2026-02-18 15:39:10 +0530

    Hero section update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
 
 const App = () => {
 
   return (
-    <Header />
+    <div>
+      <Header />
+      <Hero />
+    </div>
   )
 }
 
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./Hero.css"
+
+const Hero = () => {
+  return (
+    <div>
+      <div>
+        <h1>We Build Digital <span>Experiences</span> That Matter</h1>
+        <p>From idea to launch — we craft websites, apps, and brands that help startups and businesses grow in the digital world.</p>
+        <button>Our Services</button>
+      </div>
+      <div>
+        <div>
+            <h1>50+</h1>
+            <p>Projects Delivered</p>
+        </div>
+        <div>
+            <h1>30+</h1>
+            <p>Happy Clients</p>
+        </div>
+        <div>
+            <h1>99%</h1>
+            <p>Client Satisfaction</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
